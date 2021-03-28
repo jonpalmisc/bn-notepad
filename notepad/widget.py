b7a329feb8ec7d2f8d142bf104863834a3e21b89
Jon Palmisciano <jp@jonpalmisc.com>
2021-03-27 23:30:17 -0400

    Initial edit/view tabs proof-of-concept

--- notepad/widget.py
+++ notepad/widget.py
@@ -4,10 +4,22 @@ from binaryninja import BinaryView
 from binaryninjaui import DockContextHandler
 
 try:
-    from PySide6.QtWidgets import QWidget, QTextEdit, QVBoxLayout
+    from PySide6.QtWidgets import (
+        QWidget,
+        QTabWidget,
+        QTextBrowser,
+        QTextEdit,
+        QVBoxLayout,
+    )
     from PySide6.QtCore import QTimer
 except ImportError:
-    from PySide2.QtWidgets import QWidget, QTextEdit, QVBoxLayout
+    from PySide2.QtWidgets import (
+        QWidget,
+        QTabWidget,
+        QTextBrowser,
+        QTextEdit,
+        QVBoxLayout,
+    )
     from PySide2.QtCore import QTimer
 
 from .editor import JMarkdownEditor
@@ -24,8 +36,14 @@ class NotepadDockWidget(QWidget, DockContextHandler):
     user an interface to edit their notes from.
     """
 
+    # Tab container
+    tab_container: QTabWidget
+
     # The actual editor widget.
     editor: JMarkdownEditor
+
+    # Viewer/content widget
+    viewer: QTabWidget
 
     # Timer for auto-saving.
     save_timer: QTimer
@@ -56,9 +74,19 @@ class NotepadDockWidget(QWidget, DockContextHandler):
         self.editor = JMarkdownEditor(self)
         self.editor.textChanged.connect(self.on_editor_text_changed)
 
+        # Create the viewer
+        self.viewer = QTextBrowser()
+        self.viewer.setOpenLinks(True)
+        self.viewer.setOpenExternalLinks(True)
+
+        # Add both widgets to a tab container
+        self.tab_container = QTabWidget()
+        self.tab_container.addTab(self.viewer, "View")
+        self.tab_container.addTab(self.editor, "Edit")
+
         # Create a simple layout for the editor and set it as the root layout.
         layout = QVBoxLayout()
-        layout.addWidget(self.editor)
+        layout.addWidget(self.tab_container)
         self.setLayout(layout)
 
         # Probably does nothing since there should be no BV when the program
@@ -74,6 +102,7 @@ class NotepadDockWidget(QWidget, DockContextHandler):
         """
 
         self.save_timer.start(1000)
+        self.viewer.setMarkdown(self.editor.toPlainText())
 
     def on_binary_view_changed(self, new_bv: BinaryView):
         """
